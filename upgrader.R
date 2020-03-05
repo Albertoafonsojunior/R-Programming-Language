f13680537726ce6fb223cc5aea7d13292808d269
Alberto Afonso Júnior <albertoafonsojunior@yahoo.com.br>
2020-03-05 10:56:15 +0200

    A step by step (screenshots) tutorial for upgrading R on Windows

---
+++ upgrader.R
@@ -0,0 +1,13 @@
+
+# If you are running R on Windows you can easily upgrade to the 
+# latest version of R using the installr package. Simply run the 
+# following code:
+
+# installing/loading the latest installr package:
+install.packages("installr"); library(installr) # install+load installr
+
+updateR() # updating R.
+
+# Running "updateR()" will detect if there is a new R version available, 
+# and if so it will download+install it (etc.). just press "next", "OK", 
+# and "Yes" on everything.
